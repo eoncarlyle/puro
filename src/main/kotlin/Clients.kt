1f4db1f34462f01dcc4c3b7e0c1baddddeb0644f
eoncarlyle <me@iainschmitt.com>
2025-09-20 23:12:19 -0500

    VLQ for key, value length

--- src/main/kotlin/Clients.kt
+++ src/main/kotlin/Clients.kt
@@ -5,15 +5,39 @@ import java.nio.channels.FileChannel
 import java.nio.channels.FileLock
 import java.nio.file.FileSystem
 import java.nio.file.FileSystems
-import java.nio.file.OpenOption
 import java.nio.file.StandardOpenOption
 import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.toJavaDuration
 
-class Record<K,V>(
+class Record<K, V>(
     val key: K,
     val value: V
 )
+
+fun ceilingDivision(dividend: Long, divisor: Long): Int = ((dividend + divisor - 1) / divisor).toInt()
+fun Long.toBuffer(bytesReq: Int): ByteBuffer = ByteBuffer.allocate(bytesReq).putLong(this)
+fun Long.get7Bit(index: Int): Byte = (this shr (index * 7) and 0x7F).toByte()
+fun Long.last7Bit(): Byte = (this and 0x7F).toByte()
+
+// https://en.wikipedia.org/wiki/Variable-length_quantity
+// Little-endian I guess
+fun Long.toVlq(): ByteBuffer {
+    val bitsReq = (Long.SIZE_BITS - this.countLeadingZeroBits()).toLong()
+    // Need to throw if the long conversion doesn't work: may not even be possible with longs
+    val bytesReq = ceilingDivision(bitsReq, 7)
+    if (bytesReq == 1) {
+        ByteBuffer.allocate(1).put(this.last7Bit())
+    }
+    // Lots of int -> long, vise versa
+    val finalBuffer = ByteBuffer.allocate(bytesReq)
+    //'Little-endian byte order allows us to support arbitrary lengths more easily,'
+    // https://github.com/apache/kafka/blob/5919762009a0bbc4948bf881a6fbd885e30527b1/trogdor/src/main/java/org/apache/kafka/trogdor/workload/SequentialPayloadGenerator.java#L44
+    for (i in 0..<bytesReq - 1) {
+        finalBuffer.put(this.get7Bit(i))
+    }
+    finalBuffer.put(this.get7Bit(bytesReq - 1))
+    return finalBuffer
+}
 
 // RandomAccessFile, FileChannel `tryLock`
 class PuroProducer(
@@ -27,8 +51,7 @@ class PuroProducer(
         val retryDelay = 10.milliseconds.toJavaDuration()
     }
 
-    fun <K,V> send(key: ByteBuffer, value: ByteBuffer): Unit {
-
+    fun <K, V> send(key: ByteBuffer, value: ByteBuffer): Unit {
         // Assuming is on active segment
         FileChannel.open(streamFilePath, StandardOpenOption.APPEND).use { channel ->
             val fileSize = channel.size()
---
+++ src/test/kotlin/ClientsTest.kt
@@ -0,0 +1,37 @@
+import com.iainschmitt.toVlq
+import org.junit.jupiter.api.Test
+import java.nio.ByteBuffer
+
+class ClientsTest {
+
+    private fun equivalent(byteBuffer: ByteBuffer, byteArray: ByteArray) = byteBuffer.array().contentEquals(byteArray)
+
+    @Test
+    fun `Saturated 1 bit VLQ`() {
+        val expected = byteArrayOf(0x7F)
+        val actual = 127L.toVlq()
+        assert(equivalent(actual, expected))
+    }
+
+    @Test
+    fun `Transition to 2 bit VLQ`() {
+        val expected = byteArrayOf(0, 1)
+        val actual = 128L.toVlq()
+        assert(equivalent(actual, expected))
+    }
+
+    @Test
+    fun `Saturated 2 bit VLQ`() {
+        val expected = byteArrayOf(0x7F, 0x7F)
+        val actual = 16383L.toVlq()
+        assert(equivalent(actual, expected))
+    }
+
+    @Test
+    fun `Transition to 3 bit VLQ`() {
+        val expected = byteArrayOf(0, 0, 1)
+        val actual = 16384L.toVlq()
+        assert(equivalent(actual, expected))
+    }
+
+}
